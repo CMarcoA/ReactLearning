b9afa9ebb5035005398da19fb80e5d2e9347f528
Claudius Marco Andrew <claudiusma2022@gmail.com>
2025-02-18 17:31:57 -0600

    Modified App.js and Deleted logo.svg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome my bros"
+  const link = "https://www.figma.com/design/Wnf99WoTWvGDPHqqod8g4E/Untitled?node-id=0-1&p=f&t=usRaKZIcAzzX0v56-0"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1> Marco is testing !!!</h1>
+        <h2> { title } </h2>
+        <h2> { "2 + 3 : "+ ( 2+3 )} </h2>
+
+        <a href ={link}>Figma Figma boy</a>
+      </div>
     </div>
   );
 }
